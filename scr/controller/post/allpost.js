92f8c1c64e17d56740ad03a5042742866a653765
won <rjsdnql123@gmail.com>
2020-07-30 21:47:16 +0900

    Fix include UserModels  in get All Post

--- scr/controller/post/allpost.js
+++ scr/controller/post/allpost.js
@@ -2,11 +2,15 @@ const sequelize = require ('../../models');
 const { Post, Comments, User } = sequelize;
 //최대 10 개의 post정보와 댓글, 유저의 정보를 가져와 준다.
 const allPost = async function(req, res) {
+    
     try{
        await Post.findAll({
            include: [
                {
                 model: Comments,
+                include: [{
+                    model: User
+                }]
                },{
                 model: User
                }
