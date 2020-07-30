9c773ee5fc212bf529e2bb3340fccd372e3fa0db
won <rjsdnql123@gmail.com>
2020-07-30 21:49:22 +0900

    Fix request params in PostId

--- scr/controller/post/getpost.js
+++ scr/controller/post/getpost.js
@@ -3,8 +3,10 @@ const { Post, Comments, User } = sequelize;
 //특정 게시글을 번호를 받으면 유저의 정보와 그 포스트의 글 댓글, 댓글쓴 유저의 정보를 가져와준다.
 const getPost = async function(req, res) {
     try{
+        const { postid } = req.query
+        console.log(postid,'post')
         await Post.findAll({
-            where: {post_Id: 1},
+            where: {post_Id: postid},
             include: [
             {
                 model: Comments,
